f4402a72a6bc329433b15cc60b72af137dbb5af7
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-08-01 22:47:18 +0530

    Add files via upload

---
+++ dem.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        // Vairiable
+
+        $name = "Kartik";
+        $age = 19;
+        $op = "<h1>Hello people!!!!</h1>";
+
+        
+        // Concatination "." (dot) is used
+
+        echo "I am " . $name . ", I am " . $age . " years old.";
+
+    ?>
+</body>
+</html>
